e964f58db19ecc08f98a57421a70e5ed8351b722
liuguocheng <945216271@qq.com>
2018-03-13 11:11:20 +0800

    User can upload topic image

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -2,12 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Handlers\ImageUploadHandler;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\TopicRequest;
-use App\Models\Topic;
-use Illuminate\Http\Request;
 use App\Models\Category;
+use App\Models\Topic;
 use Auth;
+use Illuminate\Http\Request;
 
 class TopicsController extends Controller
 {
@@ -30,7 +31,7 @@ class TopicsController extends Controller
     public function create(Topic $topic)
     {
         $categories = Category::all();
-        return view('topics.create_and_edit', compact('topic','categories'));
+        return view('topics.create_and_edit', compact('topic', 'categories'));
     }
 
     public function store(TopicRequest $request, Topic $topic)
@@ -62,5 +63,27 @@ class TopicsController extends Controller
         $topic->delete();
 
         return redirect()->route('topics.index')->with('message', 'Deleted successfully.');
+    }
+
+    public function uploadImage(Request $request, ImageUploadHandler $uploader)
+    {
+        // 初始化返回数据，默认是失败的
+        $data = [
+            'success'   => false,
+            'msg'       => '上传失败!',
+            'file_path' => '',
+        ];
+        // 判断是否有上传文件，并赋值给 $file
+        if ($file = $request->upload_file) {
+            // 保存图片到本地
+            $result = $uploader->save($request->upload_file, 'topics', \Auth::id(), 1024);
+            // 图片保存成功的话
+            if ($result) {
+                $data['file_path'] = $result['path'];
+                $data['msg']       = '上传成功!';
+                $data['success']   = true;
+            }
+        }
+        return $data;
     }
 }
