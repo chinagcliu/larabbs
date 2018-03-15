d3be63bec67c4ba5119420fab3f92996ea210edc
liuguocheng <945216271@qq.com>
2018-03-15 11:42:58 +0800

    Admin can manage users

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -27,9 +27,9 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        \Horizon::auth(function ($request) {
-            // 是否是站长
-            return \Auth::user()->hasRole('Founder');
-        });
+        // \Horizon::auth(function ($request) {
+        //     // 是否是站长
+        //     return \Auth::user()->hasRole('Founder');
+        // });
     }
 }
